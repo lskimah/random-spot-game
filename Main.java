48744680b0060b9ef2cc88b949c8c9b8688074cf
lskim <lskim@loyola.edu>
2024-10-01 18:22:18 -0400

    add directions

--- Main.java
+++ Main.java
@@ -12,7 +12,13 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        System.out.print("Enter player name: ");
+        System.out.println("random-spot-game:\nUser(you) and computer compete by rolling a 6-side die to select " +
+                        "which of the 6 spots to add their name to it.\nThere is a small chance that the user can select "
+                + "which spot to add their name.\n\n" + "1. User always goes first and then the computer\n2. Each " +
+                "player rolls a die\n3. One point is given if adding a name to the spot was successful\n4. User will be" +
+                " asked if they want to exit the game after each turn\n---------\n");
+
+        System.out.print("Enter your name: ");
         String userName = scan.nextLine();
 
         // Create and start game
