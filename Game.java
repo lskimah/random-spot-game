8cf7e7d70f15e9dbe78727717f258c75fa56f08a
lskim <lskim@loyola.edu>
2024-10-01 16:18:03 -0400

    add game interface

---
+++ Game.java
@@ -0,0 +1,9 @@
+/**
+ * Game Interface for methods related to game.
+ * @author lskimah
+ */
+public interface Game {
+    void start();
+    void printScoreBoard();
+    boolean isEnd();
+}
---
+++ Main.java
@@ -0,0 +1,15 @@
+/**
+ * Main class for random-race.
+ * @author lskimah
+ */
+public class Main {
+
+    /**
+     *  Main method.
+     * @param args arguments part of main method.
+     */
+    public static void Main(String[] args) {
+
+
+    }
+}
